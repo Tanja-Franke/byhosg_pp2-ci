79314b047e1eef2c937d8b011b0f8e48ec90aa5b
Tanja Franke <wwicxcut@students.codeinstitute.net>
2024-12-25 18:19:52 +0000

    Add a wordlist as an array for the game

---
+++ assets/js/wordlist.js
@@ -0,0 +1,79 @@
+
+//array for the game
+const bibleWordList=[
+    {
+        word: "jehovah",
+        hint: "Hebrew name of God."
+    },
+    {
+        word: "jireh",
+        hint: "God, the Provider"
+    },
+    {
+        word: "nissi",
+        hint: "God, our Banner"
+    },
+    {
+        word: "rapha",
+        hint: "God, the Healer"
+    },
+    {
+        word: "shaddai",
+        hint: "God, Almighty"
+    },
+    {
+        word: "genesis",
+        hint: "Old Testament, it tells about the beginning of everything!"
+    },
+    {
+        word: "matthew",
+        hint: "New Testament, one of the Gospels, named after one Jesus´ deciples."
+    },
+    {
+        word: "psalms",
+        hint: "Old Testament, containing hymns, prayers and worship"
+    },
+    {
+        word: "revelations",
+        hint: "New Testament, telling about the end time and final hope for believers."
+    },
+    {
+        word: "corinthians",
+        hint: "New Testament, Paul urges to love and to repent and turn from wicked ways."
+    },
+    { word:"daniel",
+        hint:"He survived the lions den."
+    },
+    { word:"david",
+        hint:"Once a shepherd, an anointed King of Israel, chased by Saul. The braves one to kill Goliath at that time."
+    },
+    { word:"ruth",
+        hint:"Loyal Moabite young widow, following her mother-in-law to Judah; there is a book in the Old Testament that is called after her."
+    },
+    { word:"mary",
+        hint:"Chosen by God to be the mother of Jesus. "
+    },
+    { word:"peter",
+        hint:"One of Jesus` deciples, once called Simon. "
+    },
+    {
+        word: "baptism",
+        hint: "One of Jesus command. It confirms the unity with Jesus´ death and resurrection"
+    },
+    {
+        word: "creation",
+        hint: "The Beginning of everything."
+    },
+    {
+        word: "ascension",
+        hint: "Jesus return to the Father."
+    },
+    {
+        word: "crucifixion",
+        hint: "This is how Jesus died."
+    },
+    {
+        word: "flood",
+        hint: "Noah was called to build the Ark due to this incident."
+    }
+];

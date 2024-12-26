f863183d44d4f301bcd61388e771656652a173ec
Tanja Franke <wwicxcut@students.codeinstitute.net>
2024-12-26 10:19:10 +0000

    Add initial JS code to initiate game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,94 +1,37 @@
+/*jshint esversion: 6 */
 //DOMContentLoad waiting for DOM to load before starting the game 
 //(https://dev.to/obere4u/domcontentloaded-vs-windowonload-9mc, love maths walkthrough)
 document.addEventListener('DOMContentLoaded', function () {
-    startGame();
-    });
-
-//Variables
-/*jshint esversion: 6 */
-
-//General variables
+    initiateGame();
+});
+//Global variables
 const wordDisplay = document.querySelector("#current");
 const houseImage = document.querySelector("#housecontainer img");
 const trackAttempts = document.querySelector("#attempts");
 const input = document.querySelector("#answerinput");
-let wordArray = [];
 let guessesList = [];
+let wordArray = [];
 let currentWord, attemptsCount;
 const maxAttempts = 6;
-//function to start game
-function startGame(){
-    attemptsCount=0
+//Initial game setting
+function initiateGame() {
+    attemptsCount=0;
+    guessesList=[];
+    getRandomWord();
+    displayWord();
     houseImage.src = "assets/images/houses/house_0.png";
-    getRandom();
-     //create blanks for the searched word
-     for (let i = 0; i < currentWord.length; i++) {
+
+}
+//get random word from external js file wordlist
+function getRandomWord(){
+    const { word, hint } = bibleWordList[Math.floor(Math.random() * bibleWordList.length)];
+    currentWord = word;
+    document.querySelector("#hints").innerText = hint;
+}
+function displayWord(){
+    //create blanks for the searched word
+    for (let i = 0; i < currentWord.length; i++) {
         wordArray += "_ ";
         wordDisplay.innerHTML = wordArray;
     }
-};
-// get random word from external js file with array called bibleWordList
-const getRandom = () => {
-const { word, hint } = bibleWordList[Math.floor(Math.random() * bibleWordList.length)];
-console.log(word, hint);
-//set to show word and hint
-currentWord = word;
-document.querySelector("#hints").innerText = hint;
-};
-//check user input
-//Code snipped adapted from 
-//https://www.geeksforgeeks.org/word-guessing-game-using-html-css-and-javascript/
-/*jshint esversion: 6 */
-//as suggested in https://stackoverflow.com/questions/27441803/why-does-jshint-throw-a-warning-if-i-am-using-const 
-function checkInput(){
-        // To check empty input
-    if (!input.value) {
-        alert("Empty Input box. Please add input letter");
-        return;
-    }
-    let letter = input.value.toLowerCase();
-
-    // Clear the input field
-    input.value = "";
-
-    // Check if the letter has already been guessed
-    if (guessesList.includes(letter)) {
-        alert("You have already guessed that letter! Try with another one.");
-        return;
-    }
-    // Add the letter to the guessed letters array
-    guessesList.push(letter);
-   
-    //Show letter in guessesList container
-    document.getElementById("guessed").innerHTML = letter;
-
-    // Update the word display based on the guessed letters
-    let updatedDisplay = "";
-    let allLettersGuessed = true;
-        for (let a = 0; a < currentWord.length; a++) {
-        if (guessesList.includes(currentWord[a])) {
-            updatedDisplay += currentWord[a] + " ";
-            attemptsCount=0;
-        } else {
-            updatedDisplay += "_ ";
-            allLettersGuessed = false;
-            }
-        }
-    document.getElementById("current").textContent = updatedDisplay;
-    //if letter guess wrong
-    if(!allLettersGuessed){
-        attemptsCount++;
-        console.log(attemptsCount);
-        houseImage.src=`assets/images/houses/house_${attemptsCount}.png`;
-    }
-    trackAttempts.innerText= `Attempts: ${attemptsCount}/ ${maxAttempts}`;
-    // Check if all letters have been guessed
-    if (allLettersGuessed) {
-        alert(`🎉 Congratulations! You build your house on solid ground! You guessed: ${currentWord} !`);
-
-        } else if (attemptsCount==maxAttempts){
-            alert("😢 Sorry, your house collapsed. Try to rebuild your house on solid ground.");
-        }else{
-            return;
-        }
-    };
+}
--- assets/js/wordlist.js
+++ assets/js/wordlist.js
@@ -23,23 +23,23 @@ const bibleWordList=[
     },
     {
         word: "genesis",
-        hint: "Book in the Old Testament, it tells about the beginning of everything!"
+        hint: "Book in the Old Testament, \n it tells about the beginning of everything!"
     },
     {
         word: "matthew",
-        hint: "Book in the New Testament, one of the Gospels, named after one Jesus´ deciples."
+        hint: "Book in the New Testament, one of the Gospels, \n named after one Jesus´ deciples."
     },
     {
         word: "psalms",
-        hint: "Book in the Old Testament, containing hymns, prayers and worship"
+        hint: "Book in the Old Testament, \n containing hymns, prayers and worship"
     },
     {
         word: "revelations",
-        hint: "Books in the New Testament, telling about the end time and final hope for believers."
+        hint: "Books in the New Testament, \n telling about the end time and final hope for believers."
     },
     {
         word: "corinthians",
-        hint: "Books in the New Testament, Paul urges to love and to repent and turn from wicked ways."
+        hint: "Books in the New Testament, \n Paul urges to love and to repent \n and turn from wicked ways."
     },
     { word:"daniel",
         hint:"He survived the lions den."
@@ -48,7 +48,7 @@ const bibleWordList=[
         hint:"Once a shepherd, an anointed King of Israel, chased by Saul. The braves one to kill Goliath at that time."
     },
     { word:"ruth",
-        hint:"Loyal Moabite young widow, following her mother-in-law to Judah; there is a book in the Old Testament that is called after her."
+        hint:"Loyal Moabite young widow, following her mother-in-law to Judah; \n there is a book in the Old Testament that is called after her."
     },
     { word:"mary",
         hint:"Chosen by God to be the mother of Jesus. "
@@ -58,7 +58,7 @@ const bibleWordList=[
     },
     {
         word: "baptism",
-        hint: "One of Jesus command. It confirms the unity with Jesus´ death and resurrection"
+        hint: "One of Jesus` command. It confirms \n the unity with Jesus´ death and resurrection"
     },
     {
         word: "creation",
